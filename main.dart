2b947b6ff64ebc0e25042ef4ba48a96161b0c20b
Devangi Sharma <81170501+devangi2001@users.noreply.github.com>
2021-04-07 02:39:21 +0530

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
 import 'package:url_launcher/url_launcher.dart';
 
@@ -20,20 +19,19 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.lightBlue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomePage(title: "order coffee"),
+      home: HomePage(),
     );
   }
 }
 
 class HomePage extends StatefulWidget {
-  HomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
+  // HomePage({Key key, this.title}) : super(key: key);
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+
   bool checkBoxValue = false;
   bool checkBoxValueb = false;
   int i = 0;
@@ -107,9 +105,13 @@ class _HomePageState extends State<HomePage> {
       setState(() {
         _total = _total + 2;
       });
-    }
-    orderr = "Name: \n" +
+    }}
+
+  void launcher() async {
+    String orderr = "Name: \n" +
         name.text +
+        "\n" +
+        "Topping: +Whipping Cream " +
         "\n" +
         top +
         "Quantity:" +
@@ -118,8 +120,13 @@ class _HomePageState extends State<HomePage> {
         "\$" +
         _total.toString() +
         " \nThank You";
+    launch("mailto:devangi@gmail.com?subject=Hi&body=$orderr");
   }
-
+// @override
+  void dispose() {
+    name.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -128,168 +135,127 @@ class _HomePageState extends State<HomePage> {
           title: Text("ORDER YOUR COFFEE"),
         ),
         body: SingleChildScrollView(
-            child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                      TextField(
-                          controller: name,
-                          keyboardType: TextInputType.name,
-                          decoration: InputDecoration(
-                            hintText: 'Name',
-                            border: OutlineInputBorder(),
-                          )),
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                TextField(
+                    controller: name,
+                    keyboardType: TextInputType.name,
+                    decoration: InputDecoration(
+                      hintText: 'Name',
+                      border: OutlineInputBorder(),
+                    )),
+                Text(
+                  " TOPPINGS ",
+                  style: TextStyle(color: Colors.black, fontSize: 20),
+                ),
+                Container(
+                  child: Row(
+                    children: [
+                      Checkbox(
+                          value: checkBoxValue,
+                          onChanged: (bool value) {
+                            print(value);
+                            setState(() {
+                              checkBoxValue = value;
+                            });
+                          }),
                       Text(
-                        " TOPPINGS ",
-                        style: TextStyle(color: Colors.black, fontSize: 20),
+                        " chocolate ",
+                        style:
+                        TextStyle(color: Colors.black, fontSize: 20),
                       ),
-                      Container(
-                        child: Row(
-                          children: [
-                            Checkbox(
-                                value: checkBoxValue,
-                                onChanged: (bool value) {
-                                  print(value);
-                                  setState(() {
-                                    checkBoxValue = value;
-                                  });
-                                }),
-                            Text(
-                              " chocolate ",
-                              style:
-                                  TextStyle(color: Colors.black, fontSize: 20),
-                            ),
-                          ],
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Row(children: [
+                    Checkbox(
+                        value: checkBoxValueb,
+                        onChanged: (bool value) {
+                          print(value);
+                          setState(() {
+                            checkBoxValueb = value;
+                          });
+                        }),
+                    Text(
+                      " Whipped Cream ",
+                      style: TextStyle(color: Colors.black, fontSize: 20),
+                    ),
+                  ]),
+                ),
+                Text(
+                  " QUANTITY ",
+                  style: TextStyle(color: Colors.black, fontSize: 20),
+                ),
+                Container(
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconButton(
+                          onPressed: changedatab,
+                          icon: Icon(Icons.remove_circle_outlined),
+                          iconSize: 50,
                         ),
-                      ),
-                      Container(
-                        child: Row(children: [
-                          Checkbox(
-                              value: checkBoxValueb,
-                              onChanged: (bool value) {
-                                print(value);
-                                setState(() {
-                                  checkBoxValueb = value;
-                                });
-                              }),
-                          Text(
-                            " Whipped Cream ",
-                            style: TextStyle(color: Colors.black, fontSize: 20),
-                          ),
-                        ]),
-                      ),
-                      Text(
-                        " QUANTITY ",
-                        style: TextStyle(color: Colors.black, fontSize: 20),
-                      ),
-                      Container(
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              IconButton(
-                                onPressed: changedatab,
-                                icon: Icon(Icons.remove_circle_outlined),
-                                iconSize: 50,
-                              ),
-                              Text("$i"),
-                              IconButton(
-                                onPressed: changedata,
-                                icon: Icon(Icons.add_circle_outlined),
-                                iconSize: 50,
-                              ),
-                            ]),
-                      ),
-                      Text(
-                        'Order Summary',
-                        style: TextStyle(color: Colors.black87, fontSize: 20),
-                      ),
-                      Visibility(
-                          maintainSize: true,
-                          maintainAnimation: true,
-                          maintainState: true,
-                          visible: viewVisible,
-                          child: Container(
-                              height: 230,
-                              width: 200,
-                              margin: EdgeInsets.only(top: 10, bottom: 10),
-                              child: Center(
-                                child: Text("$orderr",
-                                    style: TextStyle(
-                                        color: Colors.brown, fontSize: 23)),
-                              ))),
-                      Center(
-                        child: ElevatedButton(
+                        Text("$i"),
+                        IconButton(
+                          onPressed: changedata,
+                          icon: Icon(Icons.add_circle_outlined),
+                          iconSize: 50,
+                        ),
+                      ]),
+                ),
+                Text(
+                  'Order Summary',
+                  style: TextStyle(color: Colors.black87, fontSize: 20),
+                ),
+                Visibility(
+                    maintainSize: true,
+                    maintainAnimation: true,
+                    maintainState: true,
+                    visible: viewVisible,
+                    child: Container(
+                        height: 230,
+                        width: 200,
+                        margin: EdgeInsets.only(top: 10, bottom: 10),
+                        child: Center(
+                          child: Text("Name: \n" + name.text + "\n" + top +
+                              "Quantity:" + i.toString() + "\nTotal:" +
+                              "\$" + _total.toString() + " \nThank You",
+                              style: TextStyle(
+                                  color: Colors.brown, fontSize: 23)),
+                        ))),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      _topp();
+                      _showOrder();
+                    },
+                    child: Text("order"),
+                  ),
+                ),
+                Container(
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        ElevatedButton(
+                          child: Text("refresh"),
                           onPressed: () {
-                            _showOrder();
-                            _topp();
+                            _reset();
                           },
-                          child: Text("order"),
                         ),
-                      ),
-                      Container(
-                          child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          ElevatedButton(
-                            child: Text("refresh"),
+                        ElevatedButton(
+                            child: Text("gmail", style: TextStyle(color: Colors.black),),
                             onPressed: () {
-                              _reset();
-                            },
-                          ),
-                          ElevatedButton(
-                            child: Text("gmail",
-                              style: TextStyle(color: Colors.lightBlue)),
-                               onPressed: () {
-                              showDialog(
-                                  context: context,
-                                  builder: (BuildContext context) {
-                                    return SimpleCustomAlert(
-                                        "Simple Custom Alert");
-                                  });
-                            },
-                          )
-                        ],
-                      )),
-                    ]))));
-  }
-}
-
-class SimpleCustomAlert extends StatelessWidget {
-  final title;
-
-  SimpleCustomAlert(this.title);
-
-  @override
-  Widget build(BuildContext context) {
-    return Dialog(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
-      child: Container(
-        height: 300,
-        child: Padding(
-          padding: const EdgeInsets.all(45.0),
-          child: Column(
-            children: [
-              Container(
-                color: Colors.white70,
-                child: ElevatedButton(
-                    child: Icon(Icons.email),
-                  onPressed: () =>
-                 launch(
-                   "mailto:devangi@gmail.com?subject=Hi&body=How are you%20plugin"),
-                )),
-              Text("Gmail"),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: Text("okay",
-                style: TextStyle(color: Colors.black)),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+                              launcher();
+                            }
+                        ),
+                      ]),
+                )
+              ],
+            ),),
+        ));
   }
 }
